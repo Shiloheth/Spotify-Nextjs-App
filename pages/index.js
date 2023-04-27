10c3facf1b37a52a53bf7a68369290b7db9da3ed
Shiloh <shilohofulue17@gmail.com>
2023-04-27 21:29:20 +0100

    tweaked the try catch block

--- pages/index.js
+++ pages/index.js
@@ -37,128 +37,107 @@ export default function HomePage() {
   }, []);
 
  
-
-
-  async function fetchSongs() {
-  const access_token = await getToken()
-  try{
-  const feedback = await fetch("https://api.spotify.com/v1/me/player/currently-playing", {
-    headers: {
-      'Authorization': 'Bearer ' + access_token
-    }
-  })
-
-  const result = await feedback.json()
-
-
- 
-
-  if (result.is_playing === false || result.currently_playing_type !== 'track' || feedback.status!==200) {
-
-  
-    const feedback = await fetch("https://api.spotify.com/v1/me/player/recently-played?limit=1", {
+ async function fetchSongs() {
+    const access_token = await getToken()
+    const headers = {
       headers: {
         'Authorization': 'Bearer ' + access_token
       }
-    })
+    }
+    try{
+      //fetch data from the currently-playing endpoint
+      const request = await fetch("https://api.spotify.com/v1/me/player/currently-playing",headers )
+      //throw new Error if the request wanst successful 
+     
     
- 
-    const result = await feedback.json()
-    setIsPlaying(false)
-    setLastSeen(result.items[0].played_at.slice(11,19))       
-    inputRef.current.src = result.items[0].track.preview_url;             
-    document.querySelector('.main').style.backgroundImage = `url(${result.items[0].track.album.images[0].url})`
-    document.querySelector('.content').style.backgroundImage = `url(${result.items[0].track.album.images[0].url})`
-    document.querySelector('.artist').textContent = result.items[0].track.artists[0].name
-    document.querySelector('.song').textContent = result.items[0].track.name
-    document.querySelector('.loader').classList.remove("loader")
+      if(!request.ok){
+        const text = await request.json()
+        throw new Error(text.error.message)}
 
-  } else{
-    setIsPlaying(true)
-    inputRef.current.src = result.item.preview_url;
-    document.querySelector('.main').style.backgroundImage = `url(${result.item.album.images[0].url})`
-    document.querySelector('.content').style.backgroundImage = `url(${result.item.album.images[0].url})`
-    document.querySelector('.artist').textContent = result.item.artists[0].name
-    document.querySelector('.song').textContent = result.item.name
-    
-    document.querySelector('.loader').classList.remove("loader");
+        const result = await request.json()
+        console.log(result)
+
+  
+      //if what is a song isn't currently being played or the playing type isnt a track fetch data from the recently played endpoint
+      if (result.is_playing === false || result.currently_playing_type !== 'track') {
+        const request = await fetch("https://api.spotify.com/v1/me/player/recently-played?limit=1",headers)
+        const result = await request.json() 
+        setIsPlaying(false)
+        setLastSeen(result.items[0].played_at.slice(11,19))      
+        inputRef.current.src = result.items[0].track.preview_url;             
+        document.querySelector('.main').style.backgroundImage = `url(${result.items[0].track.album.images[0].url})`
+        document.querySelector('.content').style.backgroundImage = `url(${result.items[0].track.album.images[0].url})`
+        document.querySelector('.artist').textContent = result.items[0].track.artists[0].name
+        document.querySelector('.song').textContent = result.items[0].track.name
+        document.querySelector('.loader').classList.remove("loader")
+
+        } 
+      else{
+        setIsPlaying(true)
+        inputRef.current.src = result.item.preview_url;
+        document.querySelector('.main').style.backgroundImage = `url(${result.item.album.images[0].url})`
+        document.querySelector('.content').style.backgroundImage = `url(${result.item.album.images[0].url})`
+        document.querySelector('.artist').textContent = result.item.artists[0].name
+        document.querySelector('.song').textContent = result.item.name
+        document.querySelector('.loader').classList.remove("loader");
+      }
+    }
+    catch(error){
+     
+      if(error.message==='The access token expired'){
+      getRefreshToken()}
+    }
   }
-  }
-  catch(error){
-    if(error.message==='The access token expired'){
-    getRefreshToken()}
-  }
-
-
-
-
-
-
-   }
-
-
-
 
 
  async function getAccessToken(){
-  const tokenUrl = "https://accounts.spotify.com/api/token";
-  const auth = Buffer.from(client_id + ':' + client_secret).toString('base64');
-  const options = {
+   const tokenUrl = "https://accounts.spotify.com/api/token";
+   const auth = Buffer.from(client_id + ':' + client_secret).toString('base64');
+   const options = {
       method: "POST",
       headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-          Authorization: `Basic ${auth}`,
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: `Basic ${auth}`,
       },
       body: `grant_type=authorization_code&redirect_uri=${redirect_uri}&code=${code}`
-  };
+    };
   
-  const response = await fetch(tokenUrl,options);
-  const data = await response.json();
- 
+    const response = await fetch(tokenUrl,options);
+    const data = await response.json();
+    console.log(data)
+    updateToken(1, data.access_token)
+    setrefreshtoken(1,data.refresh_token)
+    getToken()
 
-  
-  updateToken(1, data.access_token)
-  setrefreshtoken(1,data.refresh_token)
-  getToken()
-
-
-
-  
- }
+  }
 
  
   return (
      <>
-      
-       <div className='main'></div> 
-       <Loader/>
-       {isPlaying?<div className='circle'>LIVE</div>:<div className='lastSeen'>Last Seen:{lastSeen}</div>}
-       <div className='container'>  
-        <ComponentWithNoSSR />
-        <CurrentDate /> 
-        <div className='content'>
-        </div>
-        <MyComponent audioref={inputRef} />
-   
-        <div className='track'>
-          <div className='song'></div>
-          <div className='artist'></div>
-      
-        </div>
+        <div className='main'></div> 
+        <Loader/>
+        {isPlaying?<div className='circle'>LIVE</div>:<div className='lastSeen'>Last Seen:{lastSeen}</div>}
+        <div className='container'>  
+          <ComponentWithNoSSR />
+          <CurrentDate /> 
+          <div className='content'></div>
+          <MyComponent audioref={inputRef} />
+          <div className='track'>
+            <div className='song'></div>
+            <div className='artist'></div>
+
+          </div>
        </div>
      </>
    );
    }          
 
 
-
-
-
-function authorization(){
-  const scope =  'user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private'
+  function authorization(){
+  const scope =  'user-read-currently-playing user-read-recently-played'
   const url = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=code&redirect_uri=${redirect_uri}&scope=${scope}`
  ;
- window.location.href = url
+  window.location.href = url
  }
 
 
@@ -175,51 +154,58 @@ function authorization(){
     body: `grant_type=refresh_token&refresh_token=${refresh_token}`
   };
 
-  try {
+  
     const res = await fetch(url, authOptions);
     const data = await res.json();
     updateToken(1,data.access_token)
-  } catch (error) {
    
-  }
 }
 
       
-async function setrefreshtoken(id,refreshtoken){
+  async function setrefreshtoken(id,refreshtoken){
  
     const { data, error } = await supabase
-      .from('RefreshToken')
-      .update({ Refreshtoken: refreshtoken })
-      .eq('id', id)
-  
+    .from('RefreshToken')
+    .update({ Refreshtoken: refreshtoken })
+    .eq('id', id)
+  }
 
-  
-    
-  
-}
-
-async function getToken() {
+  async function getToken() {
   let { data } = await supabase.from('Token').select()
   return data[0].accessTokens
-}
+  }
 
-async function getRefresh() {
+  async function getRefresh() {
 
   let { data } = await supabase.from('RefreshToken').select()
   return data[0].Refreshtoken
-}
+  }
 
- async function updateToken (id, newToken) {
+  async function updateToken (id, newToken) {
   const { data, error } = await supabase
-    .from('Token')
-    .update({ accessTokens: newToken })
-    .eq('id', id)
+  .from('Token')
+  .update({ accessTokens: newToken })
+  .eq('id', id)
 
 
+  }
+
+
+
+
+
+
+async function fetchRecipes() {
+  try {
+    const response = await fetch('https://api.spoonacular.com/recipes/complexSearch');
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      throw new Error(response.status);
+    }
+  } catch (error) {
+    console.error(error);
+    // handle error
+  }
 }
-
-
-
-
-
-
